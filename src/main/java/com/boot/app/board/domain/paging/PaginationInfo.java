4dc643785384d151406e7b3861d964b95d31bc4c
halfbasement <anyang0710@naver.com>
2022-01-05 13:54:42 +0900

    paging

--- src/main/java/com/boot/app/board/domain/paging/PaginationInfo.java
+++ src/main/java/com/boot/app/board/domain/paging/PaginationInfo.java
@@ -5,29 +5,29 @@ import lombok.Data;
 @Data
 public class PaginationInfo {
 
-    /** 페이징 계산에 필요한 파라미터들이 담긴 클래스 */
+  /*  *//** 페이징 계산에 필요한 파라미터들이 담긴 클래스 *//*
     private Criteria criteria;
 
-    /** 전체 데이터 개수 */
+    *//** 전체 데이터 개수 *//*
     private int totalRecordCount;
 
-    /** 전체 페이지 개수 */
+    *//** 전체 페이지 개수 *//*
     private int totalPageCount;
 
-    /** 페이지 리스트의 첫 페이지 번호 */
+    *//** 페이지 리스트의 첫 페이지 번호 *//*
     private int firstPage;
 
-    /** 페이지 리스트의 마지막 페이지 번호 */
+    *//** 페이지 리스트의 마지막 페이지 번호 *//*
     private int lastPage;
 
-    /** SQL의 조건절에 사용되는 첫 RNUM */
+    *//** SQL의 조건절에 사용되는 첫 RNUM *//*
     private int firstRecordIndex;
 
 
-    /** 이전 페이지 존재 여부 */
+    *//** 이전 페이지 존재 여부 *//*
     private boolean hasPreviousPage;
 
-    /** 다음 페이지 존재 여부 */
+    *//** 다음 페이지 존재 여부 *//*
     private boolean hasNextPage;
 
     public PaginationInfo(Criteria criteria) {
@@ -54,29 +54,29 @@ public class PaginationInfo {
 
     private void calculation() {
 
-        /* 전체 페이지 수 (현재 페이지 번호가 전체 페이지 수보다 크면 현재 페이지 번호에 전체 페이지 수를 저장) */
+        *//* 전체 페이지 수 (현재 페이지 번호가 전체 페이지 수보다 크면 현재 페이지 번호에 전체 페이지 수를 저장) *//*
         totalPageCount = ((totalRecordCount - 1) / criteria.getRecordsPerPage()) + 1;
         if (criteria.getCurrentPageNo() > totalPageCount) {
             criteria.setCurrentPageNo(totalPageCount);
         }
 
-        /* 페이지 리스트의 첫 페이지 번호 */
+        *//* 페이지 리스트의 첫 페이지 번호 *//*
         firstPage = ((criteria.getCurrentPageNo() - 1) / criteria.getPageSize()) * criteria.getPageSize() + 1;
 
-        /* 페이지 리스트의 마지막 페이지 번호 (마지막 페이지가 전체 페이지 수보다 크면 마지막 페이지에 전체 페이지 수를 저장) */
+        *//* 페이지 리스트의 마지막 페이지 번호 (마지막 페이지가 전체 페이지 수보다 크면 마지막 페이지에 전체 페이지 수를 저장) *//*
         lastPage = firstPage + criteria.getPageSize() - 1;
         if (lastPage > totalPageCount) {
             lastPage = totalPageCount;
         }
 
-        /* SQL의 조건절에 사용되는 첫 RNUM */
+        *//* SQL의 조건절에 사용되는 첫 RNUM *//*
         firstRecordIndex = (criteria.getCurrentPageNo() - 1) * criteria.getRecordsPerPage();
 
-        /* 이전 페이지 존재 여부 */
+        *//* 이전 페이지 존재 여부 *//*
         hasPreviousPage = firstPage != 1;
 
-        /* 다음 페이지 존재 여부 */
+        *//* 다음 페이지 존재 여부 *//*
         hasNextPage = (lastPage * criteria.getRecordsPerPage()) < totalRecordCount;
     }
-
+*/
 }
