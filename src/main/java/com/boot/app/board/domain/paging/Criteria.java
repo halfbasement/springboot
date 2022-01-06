43837616695c495ddfa9cde760db0c1d87a256b0
halfbasement <anyang0710@naver.com>
2022-01-06 23:56:46 +0900

    paging

--- src/main/java/com/boot/app/board/domain/paging/Criteria.java
+++ src/main/java/com/boot/app/board/domain/paging/Criteria.java
@@ -10,14 +10,8 @@ public class Criteria {
     /** 페이지당 출력할 데이터 개수 */
     private int recordsPerPage;
 
-    /** 화면 하단에 출력할 페이지 사이즈 */
     private int pageSize;
 
-    /** 검색 키워드 */
-    private String searchKeyword;
-
-    /** 검색 유형 */
-    private String searchType;
 
     public Criteria() {
         this.currentPageNo = 1;
